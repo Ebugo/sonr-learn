aa69ab8965193dce2790556279e0497d3f2482f8
Gospel Chinyereugo <gospelokpara@gmail.com>
2022-07-11 10:17:36 +0100

    --feat: add dummy data, style fix

---
+++ data/events.ts
@@ -0,0 +1,27 @@
+import {Event} from "../assets";
+
+const events = [
+  {
+    title: "Development from 0 to 1",
+    desc: "The fastest way to build an application on the decentralized web",
+    time: "4:00pm (GMT+1)",
+    image: Event,
+    date: "28th June, 2022"
+  },
+  {
+    title: "Development from 0 to 1",
+    desc: "The fastest way to build an application on the decentralized web",
+    time: "4:00pm (GMT+1)",
+    image: Event,
+    date: "28th June, 2022"
+  },
+  {
+    title: "Development from 0 to 1",
+    desc: "The fastest way to build an application on the decentralized web",
+    time: "4:00pm (GMT+1)",
+    image: Event,
+    date: "28th June, 2022"
+  },
+]
+
+export { events };
